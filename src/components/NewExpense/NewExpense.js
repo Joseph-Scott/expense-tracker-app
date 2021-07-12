ad6b0e78961ba36c78b97b48465e8c29adfaeda9
Joe Scott <jbs198585@gmail.com>
2021-07-12 07:05:15 -0400

    Child to parent component communication

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -4,9 +4,19 @@ import ExpenseForm from './ExpenseForm';
 import './NewExpense.css';
 
 const NewExpense = () => {
-  return <div className="new-expense">
-    <ExpenseForm />
-  </div>
-}
+  const saveExpenseDataHandler = (enteredExpenseData) => {
+    const expenseData = {
+      ...enteredExpenseData,
+      id: Math.random().toString()
+    };
+    console.log(expenseData);
+  };
+
+  return (
+    <div className="new-expense">
+      <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
+    </div>
+  );
+};
 
 export default NewExpense;
