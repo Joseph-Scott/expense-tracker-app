a4d0ac803d4c32d72be7d643751891d99d783b1d
Joe Scott <jbs198585@gmail.com>
2021-08-16 06:19:57 -0400

    Using stateful lists bug

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -14,7 +14,7 @@ const Expenses = (props) => {
 
   return (
     <div>
-      <Card className="expenses">
+      <Card className='expenses'>
         <ExpensesFilter 
           selected={filteredYear} 
           onChangeFilter={filterChangeHandler}
